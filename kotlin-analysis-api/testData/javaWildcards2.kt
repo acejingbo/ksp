1080402de1b7c5c44c3cffc82aa75f0a206f2f40
Jingbo Yang <jingbo@meta.com>
2024-12-23 17:26:48 -0500

    Add java wildcard test to both ksp1 and ksp2

--- kotlin-analysis-api/testData/javaWildcards2.kt
+++ kotlin-analysis-api/testData/javaWildcards2.kt
@@ -73,12 +73,95 @@
 // suppressJvmWildcard : List<Number>
 // <init> : VarianceSubjectSuppressed<Any?>
 // R : Any?
+// VarianceSubject : Any
+// R : Any?
+// propWithFinalType : String
+// propWithFinalType.getter() : String
+// value : String
+// propWithOpenType : Number
+// propWithOpenType.getter() : Number
+// value : Number
+// propWithFinalGeneric : List<String>
+// propWithFinalGeneric.getter() : List<String>
+// value : List<String>
+// propWithOpenGeneric : List<Number>
+// propWithOpenGeneric.getter() : List<Number>
+// value : List<out Number>
+// propWithTypeArg : R
+// propWithTypeArg.getter() : R
+// value : R
+// propWithTypeArgGeneric : List<R>
+// propWithTypeArgGeneric.getter() : List<R>
+// value : List<out R>
+// propWithOpenTypeButSuppressAnnotation : Number
+// propWithOpenTypeButSuppressAnnotation.getter() : Number
+// value : Number
+// list2 : List<out Any?>
+// list1 : List<Any?>
+// list3 : List<out R>
+// listTypeArg : List<R>
+// list4 : List<out Number>
+// listTypeArgNumber : List<Number>
+// list5 : List<String>
+// listTypeArgString : List<String>
+// list6 : List<out MyEnum>
+// listTypeArgEnum : List<MyEnum>
+// list7 : List<out [@kotlin.jvm.JvmWildcard] String>
+// explicitJvmWildcard : List<out [@kotlin.jvm.JvmWildcard] String>
+// list8 : List<out [@kotlin.jvm.JvmSuppressWildcards] Number>
+// explicitJvmSuppressWildcard_OnType : List<[@kotlin.jvm.JvmSuppressWildcards] Number>
+// list9 : [@kotlin.jvm.JvmSuppressWildcards] List<Number>
+// explicitJvmSuppressWildcard_OnType2 : [@kotlin.jvm.JvmSuppressWildcards] List<Number>
+// starList : List<out Any?>
+// typeArgList : List<out R>
+// numberList : List<out Number>
+// stringList : List<String>
+// enumList : List<out MyEnum>
+// jvmWildcard : List<out [@kotlin.jvm.JvmWildcard] String>
+// suppressJvmWildcard : List<out [@kotlin.jvm.JvmSuppressWildcards] Number>
+// <init> : VarianceSubject<R>
+// R : Any?
 // END
 
 enum class MyEnum()
 
 @JvmSuppressWildcards
 class VarianceSubjectSuppressed<R>(
+    starList: List<*>,
+    typeArgList: List<R>,
+    numberList: List<Number>,
+    stringList: List<String>,
+    enumList: List<MyEnum>,
+    jvmWildcard: List<@JvmWildcard String>,
+    suppressJvmWildcard: List<@JvmSuppressWildcards Number>
+) {
+    var propWithFinalType: String = ""
+    var propWithOpenType: Number = 3
+    var propWithFinalGeneric: List<String> = TODO()
+    var propWithOpenGeneric: List<Number> = TODO()
+    var propWithTypeArg: R = TODO()
+    var propWithTypeArgGeneric: List<R> = TODO()
+    @JvmSuppressWildcards
+    var propWithOpenTypeButSuppressAnnotation: Number = 3
+    fun list1(list2: List<*>): List<*> { TODO() }
+    fun listTypeArg(list3: List<R>): List<R> { TODO() }
+    fun listTypeArgNumber(list4: List<Number>): List<Number> { TODO() }
+    fun listTypeArgString(list5: List<String>): List<String> { TODO() }
+    fun listTypeArgEnum(list6: List<MyEnum>): List<MyEnum> { TODO() }
+    fun explicitJvmWildcard(
+        list7: List<@JvmWildcard String>
+    ): List<@JvmWildcard String> { TODO() }
+
+    fun explicitJvmSuppressWildcard_OnType(
+        list8: List<@JvmSuppressWildcards Number>
+    ): List<@JvmSuppressWildcards Number> { TODO() }
+
+    fun explicitJvmSuppressWildcard_OnType2(
+        list9: @JvmSuppressWildcards List<Number>
+    ): @JvmSuppressWildcards List<Number> { TODO() }
+}
+
+class VarianceSubject<R>(
     starList: List<*>,
     typeArgList: List<R>,
     numberList: List<Number>,
